e8d122a11248891b2c6db519e857b105668069c9
a.beliauskas <arnoldas.beliauskas@adform.com>
2019-03-11 11:08:38 +0200

    folder structure changed, additional packages added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './application/components/App';
+import { Provider } from 'react-redux'
+import configureStore from './application/store';
+
+ReactDOM.render(
+	<Provider store={configureStore()}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
