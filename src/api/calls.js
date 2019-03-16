8057a43c11c0faf64efeec6a3f014e58fe2f4a6e
a.beliauskas <arnoldas.beliauskas@adform.com>
2019-03-16 13:11:32 +0200

    authorization to each request added

--- src/api/calls.js
+++ src/api/calls.js
@@ -1,32 +1,26 @@
 import * as superagent from 'superagent'
 
 import { GITHUB_API } from '../utils/utils'
+import { token } from './../config'
+
+const agent = superagent.agent().set('Authorization', 'token ' + token)
+
 
 export const getRepos = (query) => {
-	return superagent
+	return agent
 		.get(`${GITHUB_API}/search/repositories?q=${query}`)
 		.then(response => response.body)
 }
 
 export const getContributors = (path) => {
-	return superagent
+	return agent
 		.get(`${GITHUB_API}/repos/${path}/contributors`)
-		.set('Authorization', 'token ' + token )
 		.then(response => response.body)
 }
 
 export const getStarredData = (path) => {
-	return superagent
+	return agent
 		.get(`${GITHUB_API}/user/starred/${path}`)
-		.set('Authorization', 'token ' + token )
 		.then(response => response)
 }
 
-
-
-export const authenticate = (token) => {
-	return superagent
-		.get(`${GITHUB_API}/user`)
-		.set('Authorization', 'token ' + token )
-		.then(response => response.body)
-}
