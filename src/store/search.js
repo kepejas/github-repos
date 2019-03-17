099c901ce8a99abdbf55e1ae1927bd069fde2683
a.beliauskas <arnoldas.beliauskas@adform.com>
2019-03-17 14:18:10 +0200

    search thunk alterations and tests added

--- src/store/search.js
+++ src/store/search.js
@@ -1,53 +1,12 @@
 import { createReducer, makeSetValueReducer } from "../utils/reducers";
-import { getRepos } from "../api/calls";
-import { addMultipleRepos, resetReposBeforeNewQuery } from "./repos";
 import { makeSetValueActionCreator } from "../utils/actionCreators";
 import { combineReducers } from "redux";
-import { setLoadingError } from "./error";
 
 const SEARCH_SET_LOADING = 'SEARCH_SET_LOADING'
 const SEARCH_SET_NO_RESULTS = 'SEARCH_SET_NO_RESULTS'
 
-const setLoading = makeSetValueActionCreator(SEARCH_SET_LOADING)
-const setNoResultsFound = makeSetValueActionCreator(SEARCH_SET_NO_RESULTS)
-
-export const loadCurrentSearchQuery = (query) => (
-	dispatch
-) => {
-
-	dispatch(resetReposBeforeNewQuery())
-	dispatch(setLoading(true))
-	dispatch(setNoResultsFound(false))
-
-	return getRepos(query)
-		.then(({ items }) => {
-
-			dispatch(setLoading(false))
-
-
-			if(!items.length) {
-				dispatch(setNoResultsFound(true))
-			} else {
-				const ids = items.map(({ id }) => id)
-
-				const state = items.map(item => ({
-					description: item.description,
-					license: item.license ? item.license.name : '',
-					language: item.language,
-					stars: item.stargazers_count,
-					forks: item.forks_count,
-					issues: item.open_issues,
-
-					fullName: item.full_name
-				}))
-
-				dispatch(addMultipleRepos(ids, state))
-			}
-		})
-		.catch(() => {
-			dispatch(setLoadingError('One or more resources failed to load'))
-		})
-}
+export const setLoading = makeSetValueActionCreator(SEARCH_SET_LOADING)
+export const setNoResultsFound = makeSetValueActionCreator(SEARCH_SET_NO_RESULTS)
 
 
 const loading = createReducer(false, () => ({
--- src/thunks/search.js
+++ src/thunks/search.js
@@ -1,4 +1,7 @@
-import { loadCurrentSearchQuery } from "../store/search";
+import { addMultipleRepos, resetReposBeforeNewQuery } from "../store/repos";
+import { getRepos } from "../api/calls";
+import { setLoadingError } from "../store/error";
+import { setLoading, setNoResultsFound } from "../store/search";
 
 export const searchThunk = (query) => (
 	dispatch
@@ -7,4 +10,42 @@ export const searchThunk = (query) => (
 	if(searchQuery && searchQuery.length > 1) {
 		dispatch(loadCurrentSearchQuery(searchQuery))
 	}
+}
+
+export const loadCurrentSearchQuery = (query) => (
+	dispatch
+) => {
+
+	dispatch(resetReposBeforeNewQuery())
+	dispatch(setLoading(true))
+	dispatch(setNoResultsFound(false))
+
+	return getRepos(query)
+		.then(({ items }) => {
+
+			dispatch(setLoading(false))
+
+
+			if(!items.length) {
+				dispatch(setNoResultsFound(true))
+			} else {
+				const ids = items.map(({ id }) => id)
+
+				const state = items.map(item => ({
+					description: item.description,
+					license: item.license ? item.license.name : '',
+					language: item.language,
+					stars: item.stargazers_count,
+					forks: item.forks_count,
+					issues: item.open_issues,
+
+					fullName: item.full_name
+				}))
+
+				dispatch(addMultipleRepos(ids, state))
+			}
+		})
+		.catch(() => {
+			dispatch(setLoadingError('One or more resources failed to load'))
+		})
 }
